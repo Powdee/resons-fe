1648889f36cfaa250dde7e9fd645500723edd947
Erik Kurjak <e.kurjak@gmail.com>
2024-08-09 12:45:10 +0200

    fix: remove msw lib and update amplify output

--- src/instrumentation.ts
+++
@@ -1,7 +0,0 @@
-export async function register() {
-  if (process.env.NEXT_RUNTIME === "nodejs") {
-    console.log("Registering instrumentation for @vibepot/mocks");
-    const { server } = await import("@vibepot/mocks/node");
-    server.listen();
-  }
-}
