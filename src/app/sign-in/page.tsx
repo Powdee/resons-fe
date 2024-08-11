b0c891f62c0a98f3058e9ed7d6478e3079d5c77b
Erik Kurjak <e.kurjak@gmail.com>
2024-08-11 11:04:47 +0200

    fix: build suspense

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -4,13 +4,16 @@ import SignIn from '@vibepot/domains/auth/sign-in';
 import { QueryClientProvider } from 'react-query';
 import queryClient from '../query-client.util';
 import { Authenticator } from '@aws-amplify/ui-react';
+import { Suspense } from 'react';
 
 export default function SignInPage() {
   return (
-    <Authenticator.Provider>
-      <QueryClientProvider client={queryClient}>
-        <SignIn />
-      </QueryClientProvider>
-    </Authenticator.Provider>
+    <Suspense>
+      <Authenticator.Provider>
+        <QueryClientProvider client={queryClient}>
+          <SignIn />
+        </QueryClientProvider>
+      </Authenticator.Provider>
+    </Suspense>
   );
 }
