ecacd94f69740bd14534b1ac09d643988634cab4
Erik Kurjak <e.kurjak@gmail.com>
2024-09-14 10:01:05 +0200

    media scroll and template

--- src/app/events/[slug]/media/[mediaId]/page.tsx
+++ src/app/events/[slug]/media/[mediaId]/page.tsx
@@ -1,277 +1,140 @@
 'use client';
 
-import {
-  Caption,
-  DownloadIcon,
-  HeartIcon,
-  LeftArrowFilledIcon,
-  ShareIcon,
-  Text,
-  Title,
-} from '@vibepot/design-system';
-import Image from 'next/image';
-import { useState, useRef, MouseEvent, TouchEvent, useEffect } from 'react';
+import { useState, useEffect, useRef, useCallback } from 'react';
+import { useRouter } from 'next/navigation';
+import { motion, AnimatePresence } from 'framer-motion';
+import { useVirtualizer } from '@tanstack/react-virtual';
+import UserMediaTemplate from '@vibepot/domains/media/template';
 
-export default function Media() {
-  const [metadata, setMetadata] = useState<number>(0); // Video duration
-  const [time, setTime] = useState<number>(0); // Current time
-  const [isDragging, setIsDragging] = useState<boolean>(false); // Track dragging state
-  const [thumbnailSrc, setThumbnailSrc] = useState<string>(''); // Store thumbnail image source
-  const timelineRef = useRef<HTMLDivElement | null>(null); // Reference to the timeline element
-  const videoRef = useRef<HTMLVideoElement | null>(null); // Reference to the video element
-  const canvasRef = useRef<HTMLCanvasElement | null>(null); // Reference to a canvas to capture the video frame
-  const debounceTimeoutRef = useRef<number | null>(null); // Store the debounce timeout ID
+const mediaList = [
+  {
+    id: '0',
+    artist: 'Jakub Hadziewicz',
+    shazam: { title: 'Song 1 (feat. Artist)', link: 'link1' },
+    likes: 120,
+    user: { avatar: '/person.jpg', name: 'Erik Kurjakovićz' },
+    date: '25 AUG',
+    description: 'Description for media 1',
+  },
+  {
+    id: '1',
+    artist: 'Another Artist',
+    shazam: { title: 'Song 2 (feat. Artist)', link: 'link2' },
+    likes: 200,
+    user: { avatar: '/person2.jpg', name: 'John Doe' },
+    date: '10 SEP',
+    description: 'Description for media 2',
+  },
+  {
+    id: '2',
+    artist: 'Another Artist 2',
+    shazam: { title: 'Song 2 (feat. Artist)', link: 'link2' },
+    likes: 200,
+    user: { avatar: '/person2.jpg', name: 'John Doe' },
+    date: '10 SEP',
+    description: 'Description for media 2',
+  },
+  {
+    id: '3',
+    artist: 'Another Artist 3',
+    shazam: { title: 'Song 2 (feat. Artist)', link: 'link2' },
+    likes: 200,
+    user: { avatar: '/person2.jpg', name: 'John Doe' },
+    date: '10 SEP',
+    description: 'Description for media 2',
+  },
+  // Add more media objects here...
+];
 
-  const verticalThreshold = 100; // Set a threshold of 100px for the y-axis to prevent accidental stopping
-
-  // Calculate the current position in percentage for the progress marker
-  const currentPositionPercentage = metadata > 0 ? (time / metadata) * 100 : 0;
-
-  // Handle video metadata loading
-  const handleLoadedMetadata = (e: Event) => {
-    const videoElement = e.target as HTMLVideoElement;
-    if (videoElement.duration > 0) {
-      setMetadata(videoElement.duration); // Set the video duration when it's available
-    }
+interface MediaProps {
+  params: {
+    slug: string;
+    mediaId: string;
   };
+}
 
-  // Ensure the video metadata is loaded after refresh
+export default function Media({ params }: MediaProps) {
+  const { slug: eventId, mediaId } = params;
+  const [currentMediaIndex, setCurrentMediaIndex] = useState(mediaId);
+  const router = useRouter();
+  const parentRef = useRef<HTMLDivElement | null>(null);
+
+  const rowVirtualizer = useVirtualizer({
+    count: mediaList.length,
+    getScrollElement: () => parentRef.current,
+    estimateSize: () => window.innerHeight, // Estimate the size of each media item in px
+    overscan: 2, // Number of items to load outside the viewport
+  });
+
+  // Update the currentMediaIndex as you scroll
   useEffect(() => {
-    const video = videoRef.current;
-    if (video) {
-      // Attach the event listener
-      video.addEventListener('loadedmetadata', handleLoadedMetadata);
-      // If the metadata is already available (in case of a fast load)
-      if (video.duration > 0) {
-        setMetadata(video.duration);
+    const handleScroll = () => {
+      const scrollTop = parentRef.current?.scrollTop || 0;
+      const viewportItems = rowVirtualizer.getVirtualItems();
+      if (viewportItems.length > 0) {
+        // Find the first fully visible item (in view)
+        const firstVisibleItem = viewportItems.find((item) => item.start >= scrollTop);
+        if (firstVisibleItem && firstVisibleItem.index !== Number(currentMediaIndex)) {
+          setCurrentMediaIndex(firstVisibleItem.index.toString()); // Update the current media index
+          // router.push(firstVisibleItem.index.toString());
+        }
       }
+    };
 
-      // Clean up the event listener on unmount
-      return () => {
-        video.removeEventListener('loadedmetadata', handleLoadedMetadata);
-      };
+    const scrollElement = parentRef.current;
+    if (scrollElement) {
+      scrollElement.addEventListener('scroll', handleScroll);
     }
-  }, []);
 
-  // Function to handle setting the time based on click or drag (works for both mouse and touch events)
-  const updateTimeFromPosition = (clientX: number) => {
-    if (!timelineRef.current || !videoRef.current) return;
-    const rect = timelineRef.current.getBoundingClientRect(); // Get the size of the timeline
-    const position = clientX - rect.left; // Get the horizontal position
-    const percentage = Math.max(0, Math.min(position / rect.width, 1)); // Keep percentage between 0 and 1
-    const newTime = percentage * metadata; // Convert percentage to time
-    setTime(newTime); // Update state with the new time
-    videoRef.current.currentTime = newTime; // Set the video time
-    videoRef.current.pause(); // Pause the video when dragging or clicking
-    captureThumbnail(newTime); // Capture the frame thumbnail
-  };
-
-  // Capture the video frame as a thumbnail using the canvas
-  const captureThumbnail = (time: number) => {
-    const video = videoRef.current;
-    const canvas = canvasRef.current;
-    if (video && canvas) {
-      video.currentTime = time;
-      const ctx = canvas.getContext('2d');
-      canvas.width = video.videoWidth;
-      canvas.height = video.videoHeight;
-      ctx?.drawImage(video, 0, 0, canvas.width, canvas.height);
-      const thumbnail = canvas.toDataURL('image/png');
-      setThumbnailSrc(thumbnail); // Set the captured thumbnail
-    }
-  };
-
-  // Debounced play function to start the video after dragging is complete
-  const debouncePlay = () => {
-    // Clear previous debounce timeout
-    if (debounceTimeoutRef.current) {
-      clearTimeout(debounceTimeoutRef.current);
-    }
-    // Debounce by waiting 300ms before playing the video
-    debounceTimeoutRef.current = window.setTimeout(() => {
-      videoRef.current?.play();
-    }, 300); // Delay before resuming playback
-  };
-
-  // Function to handle clicks on the timeline (mouse)
-  const handleTimelineClick = (e: MouseEvent<HTMLDivElement>) => {
-    updateTimeFromPosition(e.clientX);
-    debouncePlay(); // Resume video playback after click
-  };
-
-  // Function to handle dragging (mouse move) with a y-axis threshold
-  const handleMouseMove = (e: MouseEvent<HTMLDivElement>) => {
-    if (isDragging && timelineRef.current) {
-      const rect = timelineRef.current.getBoundingClientRect();
-      const withinYThreshold =
-        e.clientY >= rect.top - verticalThreshold && e.clientY <= rect.bottom + verticalThreshold;
-
-      if (withinYThreshold) {
-        updateTimeFromPosition(e.clientX); // Only update time if within the y-threshold
+    return () => {
+      if (scrollElement) {
+        scrollElement.removeEventListener('scroll', handleScroll);
       }
-    }
-  };
+    };
+  }, [currentMediaIndex, router, rowVirtualizer]);
 
-  // Function to handle dragging (touch move) with a y-axis threshold for mobile
-  const handleTouchMove = (e: TouchEvent<HTMLDivElement>) => {
-    if (isDragging && timelineRef.current) {
-      const rect = timelineRef.current.getBoundingClientRect();
-      const touch = e.touches[0];
-      const withinYThreshold =
-        touch.clientY >= rect.top - verticalThreshold &&
-        touch.clientY <= rect.bottom + verticalThreshold;
-
-      if (withinYThreshold) {
-        updateTimeFromPosition(touch.clientX); // Only update time if within the y-threshold
-      }
-    }
-  };
-
-  // Function to handle starting the drag (mouse down)
-  const handleMouseDown = (e: MouseEvent<HTMLDivElement>) => {
-    setIsDragging(true);
-    updateTimeFromPosition(e.clientX); // Set the time when drag starts
-  };
-
-  // Function to handle starting the drag (touch start) for mobile
-  const handleTouchStart = (e: TouchEvent<HTMLDivElement>) => {
-    setIsDragging(true);
-    const touch = e.touches[0];
-    updateTimeFromPosition(touch.clientX); // Set the time when drag starts
-  };
-
-  // Function to handle stopping the drag (mouse up)
-  const handleMouseUp = (e: MouseEvent<HTMLDivElement>) => {
-    setIsDragging(false);
-    debouncePlay(); // Resume video playback after drag ends
-  };
-
-  // Function to handle stopping the drag (touch end) for mobile
-  const handleTouchEnd = (e: TouchEvent<HTMLDivElement>) => {
-    setIsDragging(false);
-    debouncePlay(); // Resume video playback after drag ends
+  const mediaAnimationVariants = {
+    initial: { opacity: 0, y: 50 }, // Initial state before entering
+    animate: { opacity: 1, y: 0, transition: { duration: 0.5 } }, // Animate to this state
+    exit: { opacity: 0, y: -50, transition: { duration: 0.5 } }, // Animate exit state
   };
 
   return (
-    <div className="relative w-full h-[100svh] bg-primary">
-      <video
-        ref={videoRef} // Reference the video element
-        className="absolute top-0 left-0 w-full h-full object-cover"
-        src="/example1.mp4"
-        autoPlay
-        loop
-        muted
-        preload="metadata" // Ensure metadata is preloaded
-        playsInline // Prevents fullscreen on mobile devices
-        onTimeUpdate={(e) => {
-          if (!isDragging) setTime(e.currentTarget.currentTime); // Only update time if not dragging
+    <div className="media-container" ref={parentRef} style={{ height: '100vh', overflowY: 'auto' }}>
+      <div
+        style={{
+          height: `${rowVirtualizer.getTotalSize()}px`,
+          position: 'relative',
         }}
-      />
-      {/* Canvas for capturing video frame thumbnails */}
-      <canvas ref={canvasRef} style={{ display: 'none' }} />
-
-      <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-between z-10">
-        <div className="flex items-center justify-between p-4">
-          <button className="p-2 bg-gray-800 bg-opacity-50 rounded-full absolute top-[10px]">
-            <LeftArrowFilledIcon />
-          </button>
-          <div className="text-center text-white w-full mt-20">
-            <Title variant="h5">Monolink</Title>
-            <Caption className="text-sm">24 AUG</Caption>
-          </div>
-          <div className="w-8" />
-        </div>
-
-        <div className="">
-          <div className="flex justify-between items-end p-20 gap-14">
-            <div className="flex flex-col gap-16">
-              <div className="flex flex-col gap-8">
-                <div className="flex items-center gap-12">
-                  <div className="flex flex-row items-center gap-2">
-                    <Image
-                      objectFit="none"
-                      className="border-2 h-[32px] border-grey-900 rounded-s first-of-type:ml-0 ml-[-16px]"
-                      src="/person.png"
-                      width={32}
-                      height={32}
-                      alt="person"
-                    />
-                  </div>
-                  <Title variant="h6">Silvia Saspor</Title>
-                </div>
-                <div className="text-white text-xs flex space-x-2">
-                  <Text variant="medium">
-                    Lorem ipsum dolor sit amet, consetur adipiscing elit, sed do eiusm View more...
-                  </Text>
-                </div>
-              </div>
-              <div className="text-white text-xs flex space-x-2">
-                <Text variant="small" className="text-grey-200">
-                  Name of song / Remix, what (2024)
-                </Text>
-              </div>
-            </div>
-
-            <div className="flex flex-col gap-32 w-fit">
-              <div className="flex flex-col items-center gap-6">
-                <HeartIcon size={22} />
-                <Text variant="medium">23</Text>
-              </div>
-              <ShareIcon size={20} />
-              <DownloadIcon size={22} />
-            </div>
-          </div>
-          <div className="p-20 pt-8 flex flex-col w-full gap-12">
-            <div className="flex flex-row w-full gap-2">
-              <div
-                ref={timelineRef}
-                className="bg-grey-900 w-[500px] h-[20px] rounded-xs relative cursor-pointer"
-                onClick={handleTimelineClick} // Handle clicks on the timeline
-                onMouseMove={handleMouseMove} // Handle dragging (mouse move) with y-threshold
-                onMouseUp={handleMouseUp} // Stop dragging when the mouse is released
-                onMouseLeave={handleMouseUp} // Stop dragging if the mouse leaves the timeline area
-                onTouchMove={handleTouchMove} // Handle touch dragging for mobile
-                onTouchStart={handleTouchStart} // Handle touch start for mobile
-                onTouchEnd={handleTouchEnd} // Stop dragging when the touch ends for mobile
+      >
+        <AnimatePresence mode="sync">
+          {rowVirtualizer.getVirtualItems().map((virtualRow) => {
+            const media = mediaList[virtualRow.index];
+            return (
+              <motion.div
+                key={media.id}
+                style={{
+                  width: '100%',
+                  transform: `translateY(${virtualRow.start}px)`,
+                }}
+                variants={mediaAnimationVariants}
+                initial="initial"
+                animate="animate"
+                exit="exit"
               >
-                {/* Marker with Thumbnail */}
-                <div
-                  className="bg-white w-[10px] h-[20px] rounded-xs absolute cursor-pointer"
-                  style={{ left: `calc(${currentPositionPercentage}%)` }} // Adjust the position of the marker
-                  onMouseDown={handleMouseDown} // Start dragging
-                  onMouseUp={handleMouseUp} // Stop dragging
-                  onTouchStart={handleTouchStart} // Handle touch start for mobile
-                  onTouchEnd={handleTouchEnd} // Stop dragging when the touch ends
-                >
-                  {/* Frame Thumbnail */}
-                  {isDragging && thumbnailSrc && (
-                    <div className="w-[96px] h-[148px] rounded-m border-2 bottom-0 border-grey-900 absolute -translate-y-[60px] -translate-x-[48px]">
-                      <Image
-                        objectFit="contain"
-                        width={96}
-                        height={148}
-                        src={thumbnailSrc}
-                        alt="video thumbnail"
-                        className="rounded-m"
-                      />
-                    </div>
-                  )}
-                </div>
-              </div>
-            </div>
-
-            <div className="flex flex-row justify-between">
-              <Text variant="small" className="text-grey-200" weight="bold">
-                {new Date(time * 1000).toISOString().substr(11, 8)} {/* Current time */}
-              </Text>
-              <Text variant="small" weight="bold">
-                {new Date(metadata * 1000).toISOString().substr(11, 8)} {/* Video duration */}
-              </Text>
-            </div>
-          </div>
-        </div>
+                <UserMediaTemplate
+                  artist={media.artist}
+                  shazam={media.shazam}
+                  likes={media.likes}
+                  user={media.user}
+                  date={media.date}
+                  description={media.description}
+                />
+              </motion.div>
+            );
+          })}
+        </AnimatePresence>
       </div>
-      <div className="vintage-effect" />
     </div>
   );
 }
